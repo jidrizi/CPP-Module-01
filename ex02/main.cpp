a7b1a49c15ad160be3f17cdb4adfc56a50a02221
jidrizi <idrizijuxhin@gmail.com>
2025-04-17 02:01:26 +0200

    ex02 was pretty simple so its already done

---
+++ ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 01:58:11 by jidrizi           #+#    #+#             */
+/*   Updated: 2025/04/17 02:00:22 by jidrizi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "Address of str: " << &str << std::endl;
+	std::cout << "Address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address of stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
