461e7a0cca84e4500a6107441c968826e941e599
jidrizi <idrizijuxhin@gmail.com>
2025-04-19 12:41:18 +0200

    just started ex05

---
+++ ex05/Harl.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 12:35:05 by jidrizi           #+#    #+#             */
+/*   Updated: 2025/04/19 12:40:44 by jidrizi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	
+	public:
+		void	complain(std::string level);
+};
+
+#endif
