0d1a6ed37a85f9ea4a70716bd424d72a8a629275
jidrizi <idrizijuxhin@gmail.com>
2025-04-19 08:29:40 +0200

    i think ex04 is pretty much done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 07:30:33 by jidrizi           #+#    #+#             */
-/*   Updated: 2025/04/19 08:00:40 by jidrizi          ###   ########.fr       */
+/*   Updated: 2025/04/19 08:28:04 by jidrizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,47 @@
 
 static int	process(std::string filename, std::string s1, std::string s2)
 {
-	std::ifstream	infile("example.txt");
-	if (!infile) {
+	std::ifstream	infile(filename);
+	if (!infile)
+	{
 		std::cerr << "Could not open input file.\n";
 		return (EXIT_FAILURE);
 	}
 	
+	int				s1_len = s1.length();
+	int				s2_len = s2_len;
+	size_t			pos = 0;
+	std::string		line;
+	while (true)
+	{
+		if (!std::getline(infile, line))
+			break;
+		while ((pos = line.find(s1, pos)) != std::string::npos)
+		{
+			line.erase(pos, s1_len);
+			line.insert(pos, s2);
+			pos += s2_len;
+		}
+	}
+
+	std::ofstream	outfile(filename + ".replace");
+	if (!outfile)
+	{
+		std::cerr << "Could not open output file.\n";
+		return (EXIT_FAILURE);
+	}
+	outfile << line;
+	outfile.close();
+	infile.close();
+
 	return (EXIT_SUCCESS);
 }
 
 int	main(int argc, char *argv[])
 {
-	std::string filename;
-	std::string s1;
-	std::string s2;
+	std::string	filename;
+	std::string	s1;
+	std::string	s2;
 
 	if (argc != 4)
 	{
@@ -44,10 +71,8 @@ int	main(int argc, char *argv[])
 		std::cerr << "Error: s1 and/or s2 cannot be empty." << std::endl;
 		return (EXIT_FAILURE);
 	}
+
 	if (process(filename, s1, s2) == EXIT_FAILURE)
-	{
-		std::cerr << "Error: Failed to process the file." << std::endl;
 		return (EXIT_FAILURE);
-	}
 	return (EXIT_SUCCESS);
 }
